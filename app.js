9db21465468e853ecd20714ed5f9b443ee5dca48
parroty-exp <parroty00+exp@gmail.com>
2022-02-12 11:42:42 +0000

    Add injection point

--- app.js
+++ app.js
@@ -3,9 +3,14 @@ const app = express()
 const port = 3000
 
 app.get('/', (_req, res) => {
-  res.send('Hello World!')
+    res.send('Hello World!')
+})
+
+app.get('/file/:fileId', (req, res) => {
+    var fileId = req.params.fileId;
+    res.sendFile("/workspace/sample-javascript-app/" + fileId);
 })
 
 app.listen(port, '0.0.0.0', () => {
-  console.log(`Example app listening at http://0.0.0.0:${port}`)
+    console.log(`Example app listening at http://0.0.0.0:${port}`)
 })
